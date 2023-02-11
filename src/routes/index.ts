5e76f4424d000d915b78f342164caf9e341b7818
Astrid Lindfors <astrid.lindfors@gmail.com>
2023-02-11 19:21:06 +0100

    possible path for authenticating all routes but register

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,9 @@
 import express from "express"
 import resource from './_router'
+import userRouter from "./User"
+import albumsRouter from "./albums"
+import photosRouter from "./photos"
+
 
 // instantiate a new router
 const router = express.Router()
@@ -13,9 +17,9 @@ router.get('/', (req, res) => {
 	})
 })
 
-
-router.use('/users', userRouter)
-router.use('/album', userRouter)
-router.use('/photos', userRouter)
+router.use('/users/:register', userRouter)
+router.use('/users', Isauthenticated, userRouter)
+router.use('/albums', Isauthenticated, albumsRouter)
+router.use('/photos', Isauthenticated, photosRouter)
 
 export default router
